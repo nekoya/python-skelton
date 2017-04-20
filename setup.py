20f61bca882d841208c4534a521ca92d38303eb2
Ryo Miyake <ryo.studiom@gmail.com>
2017-04-20 16:18:02 +0900

    Add dummy setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='foo',
+    packages=['foo'],
+    version='0.0.0',
+    author='',
+    author_email='',
+    description=""
+                "",
+    long_description=open('README.md').read(),
+    url='',
+    license='MIT',
+    tests_require=[],
+    test_suite='',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
